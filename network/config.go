56fd391eecaf619de1b051f1365318828aea45d8
Alex Godwin <alexkgodwin@gmail.com>
2018-11-27 15:34:11 -0500

    Removed duplicate config file

--- network/config.go
+++
@@ -1,8 +0,0 @@
-package network
-
-import "github.com/gladiusio/legion/utils"
-
-// LegionConfig is a config object for the legion network
-type LegionConfig struct {
-	BindAddress utils.LegionAddress
-}
